bb2c7a65d8d15223300af71813074dc1cb9aa8fb
ㄌㄌㄌㄌㄌ <68552534+nina16448@users.noreply.github.com>
2021-02-25 00:35:25 +0800

    Update W4-1.c

--- 109-2/W4-1.c
+++ 109-2/W4-1.c
@@ -1,5 +1,4 @@
 /*  probID: W4-1-Expression  */
-//#include<stdio.h>
 
 int Calculation(int n, int type[], int integers[], char symbols[], int begin, int end);
 int expr_evaluation( int n, int type[], int integers[], char symbols[] );
@@ -32,7 +31,6 @@ int Calculation(int n, int type[], int integers[], char symbols[], int begin, in
             }
         }
     }
-    //printf("%d %d\n", flag, final);
     
     if(flag == 1 || flag == -1)
         return Calculation(n, type, integers, symbols, begin, final) + flag * Calculation(n, type, integers, symbols, final + 1, end);
@@ -41,43 +39,3 @@ int Calculation(int n, int type[], int integers[], char symbols[], int begin, in
     if(flag == 2) 
         return Calculation(n, type, integers, symbols, begin, final) / Calculation(n, type, integers, symbols, final + 1, end);
 }
-
-/*
-
-int expr_evaluation(int n, int type[], int integers[], char symbols[]);
-
-int main()
-{
-
-    int times,n,ans;
-    scanf("%d", &times);
-
-    int type[10001];
-    int integers[10001];
-    char symbols[10001];
-
-    int i, j;
-    for(i=0;i<times;i++)
-    {
-
-        scanf("%d", &n);
-
-        for(j=0;j<n;j++)
-            scanf("%d", &type[j]);
-            
-        for(j=0;j<n;j++)
-            scanf("%d", &integers[j]);
-            
-        for(j=0;j<n;j++)
-            scanf(" %c", &symbols[j]);
-
-        ans = expr_evaluation(n,type,integers,symbols);
-
-        printf("%d\n", ans);
-
-    }
-    
-
-    return 0;
-}
-*/
