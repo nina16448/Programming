cc708afde496448400dead5dee1dfd38acea79f1
ㄌㄌㄌㄌㄌ <68552534+nina16448@users.noreply.github.com>
2021-03-28 13:50:46 +0800

    Update A_BackPack.cpp

--- 競程/2020.12.11_Brute force 暴力搜尋法/A_BackPack.cpp
+++ 競程/2020.12.11_Brute force 暴力搜尋法/A_BackPack.cpp
@@ -1,16 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,x;
-long long int maxx=0;
+int n, x;
+long long int maxx = 0;
 struct arr{
     long long int v;
     long long int w;
 };
 
-void DFS(int level,long long int w,long long int rec,struct arr A[]){
+void DFS(int level, long long int w, long long int rec, struct arr A[]){
     if(w > x) return;
-    if(level==n){
-        if(rec>=maxx) maxx=rec;
+    if(level == n){
+        if(rec >= maxx) maxx = rec;
         return;
     }
     DFS(level+1, w+A[level].w, rec+A[level].v, A);
@@ -20,13 +20,13 @@ void DFS(int level,long long int w,long long int rec,struct arr A[]){
 
 int main(){
     struct arr A[21];
-    scanf("%d %d",&n,&x);
-    for(int i=0; i<n; i++){
-        scanf("%lld %lld",&A[i].v ,&A[i].w);
+    scanf("%d %d", &n, &x);
+    for(int i = 0; i < n; i++){
+        scanf("%lld %lld", &A[i].v , &A[i].w);
     }
     DFS(0, 0, 0, A);
 
-    printf("%lld",maxx);
+    printf("%lld", maxx);
 
     return 0;
 }
