907c21a05848ba1161a9a04523e5b21e6cc7e070
ㄌㄌㄌㄌㄌ <68552534+nina16448@users.noreply.github.com>
2021-03-28 13:51:47 +0800

    Update B_Subset.cpp

--- 競程/2020.12.11_Brute force 暴力搜尋法/B_Subset.cpp
+++ 競程/2020.12.11_Brute force 暴力搜尋法/B_Subset.cpp
@@ -3,23 +3,24 @@ using namespace std;
 int arr[10000];
 int n;
 int ans;
-void DFS(int level,int sum){
-    if(sum>1000) return;
-    if(level==n){
+void DFS(int level, int sum){
+    if(sum > 1000) return;
+    if(level == n){
         ans++;
         return;
     }
-    DFS(level+1,sum+arr[level]);
-    DFS(level+1,sum);
+    
+    DFS(level + 1, sum + arr[level]);
+    DFS(level + 1, sum);
 }
 
 int main(){
     
-    cin>>n;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    DFS(0,0);
-    cout<<ans<<"\n";
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    DFS(0, 0);
+    cout << ans << "\n";
     return 0;
 }
