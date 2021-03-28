98ead837feaf9b7c8778b4f99d31d01bca1881f0
ㄌㄌㄌㄌㄌ <68552534+nina16448@users.noreply.github.com>
2021-03-28 13:54:16 +0800

    Update D_Permutation.cpp

--- 競程/2020.12.11_Brute force 暴力搜尋法/D_Permutation.cpp
+++ 競程/2020.12.11_Brute force 暴力搜尋法/D_Permutation.cpp
@@ -6,25 +6,25 @@ int n;
 bool used[9];
 
 void DFS(int level){
-    if(level==n){
-        for(auto x:v)
-            cout<<x<<" ";
-        cout<<"\n";
+    if(level == n){
+        for(auto x: v)
+            cout << x << " ";
+        cout << "\n";
     }
-    for(int i=1;i<=n;i++){
+    for(int i = 1; i <= n; i++){
         if(used[i]) continue;
         
-        used[i]=1;
+        used[i] = 1;
         v.push_back(i);
-        DFS(level+1);
+        DFS(level + 1);
 
-        used[i]=0;
+        used[i] = 0;
         v.pop_back();
     }
 }
 
 int main(){
-    cin>>n;
+    cin >> n;
     DFS(0);
     return 0;
 }
