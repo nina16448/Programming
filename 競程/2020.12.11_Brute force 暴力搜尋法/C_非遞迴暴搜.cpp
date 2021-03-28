9a8d0a6b55d8d0c38d4b0ada18161195e96780fc
ㄌㄌㄌㄌㄌ <68552534+nina16448@users.noreply.github.com>
2021-03-28 13:53:35 +0800

    Update C_非遞迴暴搜.cpp

--- 競程/2020.12.11_Brute force 暴力搜尋法/C_非遞迴暴搜.cpp
+++ 競程/2020.12.11_Brute force 暴力搜尋法/C_非遞迴暴搜.cpp
@@ -9,29 +9,29 @@ struct point{
 
 double dis(int a, int b, struct point p[]){
     double X,Y;
-    X= p[a].x - p[b].x;
-    Y= p[a].y - p[b].y;
-    return sqrt((X*X)+(Y*Y));
+    X = p[a].x - p[b].x;
+    Y = p[a].y - p[b].y;
+    return sqrt( (X * X) + (Y * Y) );
 }
 
 int main(){
-    double min=9999999;
-    scanf("%d",&n);
-    //cin >> n;
+    double min = 9999999;
+    scanf("%d", &n);
     struct point p[n];
-    for(int i=0;i<n;i++){
-        //cin >> p[i].x >> p[i].y;
+    
+    for(int i = 0; i < n; i++){
         scanf("%lf %lf", &p[i].x, &p[i].y);
     }
-    min=dis(0,1,p);
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==j) continue;
-            double rec=dis(i,j,p);
-            if(rec<min) min=rec;
+    min = dis(0, 1, p);
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(i == j) continue;
+            
+            double rec = dis(i, j, p);
+            
+            if(rec < min) min = rec;
         }
     }
-    printf("%.7f",min);
-    //cout << min << "\n";
+    printf("%.7f", min);
     return 0;
 }
